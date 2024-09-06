55f446c6eb113b38e06307e0d127ddcca6fb89fc
isoppp <prog@d2deck.com>
2024-09-07 01:52:31 +0900

    setup vitest which can use playwright as well

---
+++ vitest.workspace.ts
@@ -0,0 +1,27 @@
+// vitest.workspace.{ts,tsx}
+import { defineWorkspace } from "vitest/config"
+
+export default defineWorkspace([
+  {
+    test: {
+      include: ["tests/unit/**/*.{test,spec}.{ts,tsx}", "**/*.n.{test,spec}.{ts,tsx}"],
+      name: "node",
+      environment: "node",
+      globals: true,
+    },
+  },
+  {
+    test: {
+      include: ["tests/browser/**/*.{test,spec}.{ts,tsx}", "**/*.b.{test,spec}.{ts,tsx}"],
+      name: "browser",
+      browser: {
+        provider: "playwright", // or 'webdriverio'
+        enabled: true,
+        name: "chromium", // browser name is required
+        headless: true,
+      },
+      environment: "browser",
+      globals: true,
+    },
+  },
+])
