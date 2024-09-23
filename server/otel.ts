01d466b9e4b3177700eacfd28a77f47d74bf5a42
isoppp <prog@d2deck.com>
2024-09-23 17:40:33 +0900

    organize packages and otel instrumentations

--- server/otel.ts
+++ server/otel.ts
@@ -1,21 +1,19 @@
 import { TraceExporter } from '@google-cloud/opentelemetry-cloud-trace-exporter'
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
-import * as opentelemetry from '@opentelemetry/sdk-node'
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express'
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
+import { WinstonInstrumentation } from '@opentelemetry/instrumentation-winston'
+import { NodeSDK } from '@opentelemetry/sdk-node'
 import p from '@prisma/instrumentation'
 import { RemixInstrumentation } from 'opentelemetry-instrumentation-remix'
 
-const sdk = new opentelemetry.NodeSDK({
+const sdk = new NodeSDK({
   traceExporter: new TraceExporter({
     keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
   }),
   instrumentations: [
-    getNodeAutoInstrumentations({
-      // we recommend disabling fs autoinstrumentation since it can be noisy
-      // and expensive during startup
-      '@opentelemetry/instrumentation-fs': {
-        enabled: false,
-      },
-    }),
+    new ExpressInstrumentation(),
+    new HttpInstrumentation(),
+    new WinstonInstrumentation(),
     new p.PrismaInstrumentation(),
     new RemixInstrumentation(),
   ],
